9f134a9803813fed59a3a9b270f52cd7d822d290
Jae-Woo Joo <wnwodn4293@naver.com>
2020-11-19 18:24:52 +0900

    [feat] input 컴포넌트 생성
    - 인풋 컴포넌트 생성
    - 인풋 컴포넌트 스토리북 생성

---
+++ fe/src/components/atoms/Input/input.stories.tsx
@@ -0,0 +1,16 @@
+import React,{useState} from 'react';
+import Input from './index';
+
+export default {
+  title: 'Atoms/Input',
+  component: Input,
+};
+
+export const Default = () => {
+  const [data,setData]= useState('');
+  const placeholder='입력!'
+  const changeEvent = (e: React.ChangeEvent<HTMLInputElement>) =>{
+      setData(e.target.value);
+  }
+  return <Input value={data} placehonder={placeholder} onChange={changeEvent} />;
+};
