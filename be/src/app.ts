da73476bd21145f049cf02fd63814dc9ffe8d454
wseungjin <bose1021@naver.com>
2020-11-23 19:05:45 +0900

    [chore] apollo server 연결

--- be/src/app.ts
+++ be/src/app.ts
@@ -1,18 +1,24 @@
 import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+
 import logger from 'morgan';
+import cors from 'cors';
 import dotenv from 'dotenv';
-import indexRouter from './routes';
 import dbStarter from './providers/dbProvider';
 
-const app = express();
-
-app.use(logger('dev'));
+import schema from './schema';
+import resolvers from './resolvers';
 
 dotenv.config();
 
-app.use('/', indexRouter);
-
+const app = express();
 const port: number = Number(process.env.PORT) || 3000;
+const server = new ApolloServer({ typeDefs: schema, resolvers });
+
+app.use(logger('dev'));
+app.use(cors());
+
+server.applyMiddleware({ app, path: '/graphql' });
 
 const booting = async () => {
   await dbStarter();
