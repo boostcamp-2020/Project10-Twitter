cdb0cf0aad027930e88b16d1b0babdc07d706ae4
wseungjin <bose1021@naver.com>
2020-11-23 16:17:15 +0900

    [feat] db 연결후 서버 실행

--- be/src/app.ts
+++ be/src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import logger from 'morgan';
 import dotenv from 'dotenv';
 import indexRouter from './routes';
+import dbStarter from './providers/dbProvider';
 
 const app = express();
 
@@ -13,6 +14,11 @@ app.use('/', indexRouter);
 
 const port: number = Number(process.env.PORT) || 3000;
 
-app.listen(port, () => {
-  console.log('Example app listening on port 3000!');
-});
+const booting = async () => {
+  await dbStarter();
+  app.listen(port, () => {
+    console.log('Example app listening on port 3000!');
+  });
+};
+
+booting();
