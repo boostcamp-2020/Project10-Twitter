bffdb60bcfe65edf6c8f22c5e35ba66a879901e9
Jae-Woo Joo <wnwodn4293@naver.com>
2020-11-24 16:31:09 +0900

    [feat] 쿼리문, 유저타입 추가 작성
    - 트윗 리스트 조회, 팔로우 팔로잉 조회 Query 작성
    - 유저 타입 작성

--- be/src/schema/index.ts
+++ be/src/schema/index.ts
@@ -1,8 +1,9 @@
-const { gql } = require('apollo-server-express');
+import { gql } from 'apollo-server-express';
 
 const typeDefs = gql`
   type Tweet {
     author_id: String
+    author: User
     content: String
     img_url_list: [String]
     parent_id: String
@@ -12,7 +13,19 @@ const typeDefs = gql`
   }
 
   type Query {
-    tweet_list: [Tweet]
+    following_tweet_list(id: String): [Tweet]
+    user_tweet_list: [Tweet]
+    following_list(id: String): [User]
+    follower_list(id: String): [User]
+  }
+
+  type User {
+    user_id: String
+    profile_img_url: String
+    comment: String
+    background_img_url: String
+    following_list: [User]
+    following_user: User
   }
 `;
 
