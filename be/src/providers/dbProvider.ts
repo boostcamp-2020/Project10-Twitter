3f585db636cfb6d0f3c4de2bd93ed5476ea5ed56
Jae-Woo Joo <wnwodn4293@naver.com>
2020-11-24 18:26:43 +0900

    [refactor] env 로 환경변수 관리
    - .env에DB주소 설정
    - NODE_ENV에 의해 개발 서버인지, 배포서버인지 구분하여 접근

--- be/src/providers/dbProvider.ts
+++ be/src/providers/dbProvider.ts
@@ -1,7 +1,10 @@
 import mongoose from 'mongoose';
 
 const dbStarter = async () => {
-  await mongoose.connect('mongodb://101.101.211.65:27017/dev', {
+  const CONNECT_URL =
+    process.env.NODE_ENV === 'development' ? process.env.DEV_DB_URL : process.env.PRO_DB_URL;
+  if (CONNECT_URL === undefined) throw Error('db connection fail');
+  await mongoose.connect(CONNECT_URL, {
     useNewUrlParser: true,
   });
 };
