2b3259e1d7f988d399bce5a9dfbddc888aa18b94
DESKTOP-J5O26G9\admin <shmallow@naver.com>
2020-11-24 22:53:31 +0900

    [refactor] Button, IconButton, Inputcontainer 컴포넌트에서 불필요한 부분 삭제 및 추가할 부분 추가

--- fe/src/components/molecules/Button/button.stories.tsx
+++ fe/src/components/molecules/Button/button.stories.tsx
@@ -32,25 +32,34 @@ export const Default = () => {
 
 export const HomeButtonWithText = () => {
   const content = text('text', 'Home');
-  return <Button icon={Home()} text={content} />;
+  return <Button icon={Home({})} text={content} />;
 };
 
 export const ExploreButtonWithText = () => {
   const content = text('text', 'Explore');
-  return <Button icon={Explore()} text={content} />;
+  return <Button icon={Explore({})} text={content} />;
 };
 
 export const NotificationsButtonWithText = () => {
   const content = text('text', 'Notifications');
-  return <Button icon={Notifications()} text={content} />;
+  return <Button icon={Notifications({})} text={content} />;
 };
 
 export const ProfilesButtonWithText = () => {
   const content = text('text', 'Profiles');
-  return <Button icon={Profiles()} text={content} />;
+  return <Button icon={Profiles({})} text={content} />;
 };
 
 export const TwitterButtonWithText = () => {
   const content = text('text', 'Twitter');
-  return <Button icon={Twitter()} text={content} />;
+  return <Button icon={Twitter({})} text={content} />;
+};
+
+export const LoginButton = () => {
+  const content = text('text', '로그인');
+  const color = 'primary';
+  const variant = 'contained';
+  const borderRadius = 50;
+
+  return <Button borderRadius={borderRadius} text={content} color={color} variant={variant} />;
 };
