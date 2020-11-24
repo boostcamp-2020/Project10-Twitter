07fafcb458d1f05853e0a42179bd6ac9b2ebaeb7
Jae-Woo Joo <wnwodn4293@naver.com>
2020-11-24 18:58:01 +0900

    [refactor] 깃허브 client id .env로 관리
    - 비밀 키 값 들을 .env 로 관리

--- be/src/services/auth.ts
+++ be/src/services/auth.ts
@@ -2,13 +2,14 @@ import axios from 'axios';
 import { userModel } from '../models';
 
 const githubLogin = async (_: any, args: any) => {
+  console.log(args);
   const { code } = args;
   const response = await axios.post(
     'https://github.com/login/oauth/access_token',
     {
       code,
-      client_id: 'e7e296232c02f15bc3ff', // 내 APP의 정보
-      client_secret: 'e70bb812ee09775f4100553b1a57e6166c72a602', // 내 APP의 정보
+      client_id: process.env.DEV_GITHUB_CLIENT_ID, // 내 APP의 정보
+      client_secret: process.env.DEV_GITHUB_CLIENT_SECRET, // 내 APP의 정보
     },
     {
       headers: {
@@ -16,13 +17,13 @@ const githubLogin = async (_: any, args: any) => {
       },
     },
   );
-
   const token = response.data.access_token;
-  const data = await axios.get('https://api.github.com/user', {
+  const { data } = await axios.get('https://api.github.com/user', {
     headers: {
       Authorization: `token ${token}`,
     },
   });
+  return { token: '123' };
 };
 
 export { githubLogin };
