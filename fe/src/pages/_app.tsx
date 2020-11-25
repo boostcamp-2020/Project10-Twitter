86a2a24773c81d842a1fd011943418d839ab251a
DESKTOP-J5O26G9\admin <shmallow@naver.com>
2020-11-25 16:22:52 +0900

    [style] _app.tsx 작성
    기본적으로 margin이 8px로 되어있어서 0px로 수정

---
+++ fe/src/pages/_app.tsx
@@ -0,0 +1,7 @@
+import React, { FunctionComponent } from 'react';
+import { AppProps } from 'next/app';
+import '../styles/global.css';
+
+const App = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
+
+export default App;
