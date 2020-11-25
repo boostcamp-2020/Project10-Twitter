f6b0caa49f7eb96b1a168db75a6a1fc09b191367
DESKTOP-J5O26G9\admin <shmallow@naver.com>
2020-11-25 19:36:51 +0900

    [feat] Oauth 페이지 작성
    로그인 페이지에서 Oauth 버튼을 눌렀을 때 callback으로 code가 전달되고, graphql을 사용해 code를 다시 서버로 보내 토큰을 받도록 한다. 도중에 에러가 생기는 경우는 로그인 페이지로 가도록 하고, 처리가 완료되면 메인페이지로 이동하도록 구현

---
+++ fe/src/pages/callback/index.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react';
+import { useMutation } from '@apollo/client';
+import { NextPageContext, NextPage } from 'next';
+import { useRouter } from 'next/router';
+import GITHUB_LOGIN from './callback';
+
+interface Props {
+  code: string | string[] | undefined;
+}
+
+const Callback: NextPage<Props> = ({ code }) => {
+  const [login, { error, data }] = useMutation(GITHUB_LOGIN);
+  const router = useRouter();
+  useEffect(() => {
+    login({ variables: { code } });
+  }, []);
+
+  if (error) router.push('/login');
+  if (data) {
+    localStorage.setItem('jwt_token', data.auth.token);
+    router.push('/');
+  }
+  return <div>loading...</div>;
+};
+
+Callback.getInitialProps = ({ query: { code } }: NextPageContext) => {
+  return { code };
+};
+export default Callback;
