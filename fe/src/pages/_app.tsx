c7cb95e8c0d5e0d69bd31474b251669e92d45747
DESKTOP-J5O26G9\admin <shmallow@naver.com>
2020-11-25 19:41:04 +0900

    [feat] Graphql API와 연동 가능하게 하기위해서 _app.tsx에 ApolloPrivider를 앱의 최상위 컴포넌트로 감싸주었고, 서버 url을 설정함.

--- fe/src/pages/_app.tsx
+++ fe/src/pages/_app.tsx
@@ -1,7 +1,23 @@
-import React, { FunctionComponent } from 'react';
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { AppProps } from 'next/app';
 import '../styles/global.css';
 
-const App = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
+const client = new ApolloClient({
+  uri: 'http://127.0.0.1:8080/graphql', // api 서버 url
+  cache: new InMemoryCache(),
+});
+
+const App = ({ Component, pageProps }: AppProps) => (
+  <ApolloProvider client={client}>
+    <Component {...pageProps} />
+  </ApolloProvider>
+);
+
+// App.getInitialProps = ({ Component, pageProps }: AppProps) => {
+//   const user;
+//   return { ...Component, ...pageProps, user };
+// };
 
 export default App;
