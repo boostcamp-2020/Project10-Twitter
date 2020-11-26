b936352ca187e7ebfe24be30ee1d9e43a1f7f6cc
Baek Jiyoung <bjyoung0@gmail.com>
2020-11-26 17:15:40 +0900

    [feat] 사용자 정보를 context로 관리하는 로직 구현

--- be/src/app.ts
+++ be/src/app.ts
@@ -8,12 +8,22 @@ import dbStarter from './providers/dbProvider';
 
 import schema from './schema';
 import resolvers from './resolvers';
+import { verifyToken } from './services/auth';
 
 dotenv.config();
 
 const app = express();
 const port: number = Number(process.env.PORT) || 3000;
-const server = new ApolloServer({ typeDefs: schema, resolvers });
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
+  context: ({ req }) => {
+    const bearerHeader = req.headers.authorization || '';
+    const token = bearerHeader.split(' ')[1];
+    const user = verifyToken(token);
+    return { user };
+  },
+});
 
 app.use(logger('dev'));
 app.use(cors());
