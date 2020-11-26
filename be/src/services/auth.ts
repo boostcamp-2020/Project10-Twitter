08361e2dbbbde0a944bf46c8fb3e13d4b9fbcfa4
Baek Jiyoung <bjyoung0@gmail.com>
2020-11-26 17:13:40 +0900

    [feat] github에서 토큰 받아 인증 구현
    - github에서 user 정보 받아오는 함수 구현
    - 받아온 user 정보 db에서 찾거나 저장하는 함수 구현
    - 토큰 생성하는 함수 구현
    - 토큰 확인하는 함수 구현

--- be/src/services/auth.ts
+++ be/src/services/auth.ts
@@ -1,15 +1,32 @@
 import axios from 'axios';
 import { userModel } from '../models';
+import jwt from 'jsonwebtoken';
 
 const githubLogin = async (_: any, args: any) => {
-  console.log(args);
   const { code } = args;
-  const response = await axios.post(
+  const githubToken = await getGithubToken(code);
+  const githubUserInfo = await getGithubUserInfo(githubToken);
+  const user = await getOurUser(githubUserInfo);
+  const signedToken = signToken(user);
+  return signedToken;
+};
+
+const getGithubToken = async (code: string) => {
+  const githubClientId =
+    process.env.NODE_ENV === 'development'
+      ? process.env.DEV_GITHUB_CLIENT_ID
+      : process.env.PRO_GITHUB_CLIENT_ID;
+  const githubClientSecret =
+    process.env.NODE_ENV === 'development'
+      ? process.env.DEV_GITHUB_CLIENT_SECRET
+      : process.env.PRO_GITHUB_CLIENT_SECRET;
+
+  const { data } = await axios.post(
     'https://github.com/login/oauth/access_token',
     {
       code,
-      client_id: process.env.DEV_GITHUB_CLIENT_ID, // 내 APP의 정보
-      client_secret: process.env.DEV_GITHUB_CLIENT_SECRET, // 내 APP의 정보
+      client_id: githubClientId,
+      client_secret: githubClientSecret,
     },
     {
       headers: {
@@ -17,13 +34,39 @@ const githubLogin = async (_: any, args: any) => {
       },
     },
   );
-  const token = response.data.access_token;
-  const { data } = await axios.get('https://api.github.com/user', {
-    headers: {
-      Authorization: `token ${token}`,
-    },
-  });
-  return { token: '123' };
+  return data.access_token;
 };
 
-export { githubLogin };
+const getGithubUserInfo = async (githubToken: string) => {
+  const { data } = await axios.get('https://api.github.com/user', {
+    headers: {
+      Authorization: `token ${githubToken}`,
+    },
+  });
+  return data;
+};
+
+const getOurUser = async (githubUserInfo: any) => {
+  let user = await userModel.findOne({ user_id: githubUserInfo.username });
+  if (!user) {
+    user = await userModel.create({
+      user_id: githubUserInfo.username,
+      name: 'test100',
+      following_list: [],
+      profile_img_url: githubUserInfo.avatar_url,
+    });
+  }
+  return user;
+};
+
+const signToken = (user: any) => {
+  const token = jwt.sign(user, process.env.JWT_SECRET_KEY!, { expiresIn: '24h' });
+  return token;
+};
+
+const verifyToken = (token: string) => {
+  const user = jwt.verify(token, process.env.JWT_SECRET_KEY!);
+  return user;
+};
+
+export { githubLogin, verifyToken };
