154c61be65adfed60f4de408bbaf2c0124185cc0
DESKTOP-J5O26G9\admin <shmallow@naver.com>
2020-11-26 20:37:44 +0900

    [feat] 메인페이지에 필요한 NewTweetContainer Organisms 컴포넌트 작성

---
+++ fe/src/components/organisms/NewTweetContainer/newTweetContainer.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import NewTweetContainer from './index';
+
+export default {
+  title: 'Organisms/NewTweetContainer',
+  component: NewTweetContainer,
+};
+
+export const Default = () => <NewTweetContainer />;
