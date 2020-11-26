62c0967f471ae9e42f4793001a708064e52df8e4
DESKTOP-J5O26G9\admin <shmallow@naver.com>
2020-11-26 20:49:15 +0900

    [refactor] LoginRightSection Organism 컴포넌트에 styled-component 적용

--- fe/src/components/organisms/LoginRightSection/index.tsx
+++ fe/src/components/organisms/LoginRightSection/index.tsx
@@ -1,42 +1,11 @@
 import React, { FunctionComponent } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { Box } from '@material-ui/core';
-import { Twitter } from '../../atoms/icons';
+import { Twitter } from '../../atoms/Icons';
 import LoginForm from '../../molecules/LoginForm';
 import InputContainer from '../../molecules/InputContainer';
 import Button from '../../molecules/Button';
-import Text from '../../atoms/Text';
-
-const useStyles = makeStyles({
-  rightSection: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '50vw',
-    height: '95vh',
-    zIndex: 1,
-    backgroundColor: 'white',
-  },
-  loginForm: {
-    position: 'absolute',
-    top: '15px',
-  },
-  joinBox: {
-    display: 'flex',
-    flexDirection: 'column',
-    maxWidth: '400px',
-    '& Button': {
-      marginTop: '15px',
-    },
-    '& p': {
-      marginTop: '15px',
-    },
-  },
-});
+import { Container, JoinBox, LoginFormContainer, StyledButton, StyledText } from './styled';
 
 const LoginRightSection: FunctionComponent = () => {
-  const classes = useStyles();
   const emailLabelValue = '이메일';
   const emailPlaceholder = '입력!';
   const emailType = 'email';
@@ -57,8 +26,8 @@ const LoginRightSection: FunctionComponent = () => {
       'https://github.com/login/oauth/authorize?client_id=03880f7398f255239678&redirect_uri=http://localhost:3000/callback';
   };
   return (
-    <Box component="div" className={classes.rightSection}>
-      <Box component="div" className={classes.loginForm}>
+    <Container component="div">
+      <LoginFormContainer component="div">
         <LoginForm>
           <InputContainer
             labelValue={emailLabelValue}
@@ -74,16 +43,16 @@ const LoginRightSection: FunctionComponent = () => {
           />
           <Button borderRadius={borderRadius} text={content} color={color} variant={variant} />
         </LoginForm>
-      </Box>
-      <Box component="div" className={classes.joinBox}>
+      </LoginFormContainer>
+      <JoinBox component="div">
         <Twitter width="40px" height="40px" />
-        <Text
+        <StyledText
           value="지금 전 세계에서 무슨 일이 일어나고 있는지 알아보세요"
           size="1.5rem"
           weight={700}
         />
-        <Button text="가입하기" color="primary" variant="outlined" borderRadius={3} />
-        <Button
+        <StyledButton text="가입하기" color="primary" variant="outlined" borderRadius={3} />
+        <StyledButton
           text="Sign in with GitHub"
           color="secondary"
           variant="outlined"
@@ -91,8 +60,8 @@ const LoginRightSection: FunctionComponent = () => {
           icon={Twitter({})}
           onClick={onClick}
         />
-      </Box>
-    </Box>
+      </JoinBox>
+    </Container>
   );
 };
 
