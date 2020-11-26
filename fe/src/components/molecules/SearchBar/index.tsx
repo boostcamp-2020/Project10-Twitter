a85644a26a843465b6a78a30d490626302eaac8b
DESKTOP-J5O26G9\admin <shmallow@naver.com>
2020-11-26 20:46:49 +0900

    [refactor] SearchBar Molecule 컴포넌트에 styled-component 방식 적용

--- fe/src/components/molecules/SearchBar/index.tsx
+++ fe/src/components/molecules/SearchBar/index.tsx
@@ -1,50 +1,32 @@
 import React, { FunctionComponent } from 'react';
-import { fade, Theme, makeStyles } from '@material-ui/core/styles';
 import { Box } from '@material-ui/core';
-import SearchIcon from '../../atoms/icons/Search';
+import { Search } from '../../atoms/Icons';
 import Input from '../../atoms/Input/index';
+import { SearchBox, SearchIconBox } from './styled';
 
 interface Props {
   placeholder?: string;
   value?: string;
   type: string;
   variant?: 'filled' | 'outlined' | 'standard' | undefined;
+  width?: string;
 }
-
-const useStyles = makeStyles((theme: Theme) => ({
-  search: {
-    display: 'flex',
-    borderRadius: '50px',
-    width: '20%',
-    backgroundColor: fade(theme.palette.common.black, 0.15),
-  },
-  searchIcon: {
-    padding: theme.spacing(0, 2),
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-}));
 
 const SearchBar: FunctionComponent<Props> = ({
   placeholder = '',
   value = '',
   type,
   variant = undefined,
-}) => {
-  const classes = useStyles();
-
-  return (
-    <Box component="div" className={classes.search}>
-      <Box component="div" className={classes.searchIcon}>
-        <SearchIcon />
-      </Box>
-      <Box component="div">
-        <Input placeholder={placeholder} type={type} variant={variant} />
-      </Box>
+  width = '',
+}) => (
+  <SearchBox width={width}>
+    <SearchIconBox>
+      <Search />
+    </SearchIconBox>
+    <Box component="div">
+      <Input placeholder={placeholder} type={type} variant={variant} value={value} />
     </Box>
-  );
-};
+  </SearchBox>
+);
 
 export default SearchBar;
