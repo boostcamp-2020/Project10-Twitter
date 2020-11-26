b2a29967ea67cb6846f11823871734a919262004
wseungjin <bose1021@naver.com>
2020-11-26 14:21:45 +0900

    [refac] 기존 join한 정보가 아닌 새로 만든정보만 제공

--- be/src/services/tweet/addTweet.ts
+++ be/src/services/tweet/addTweet.ts
@@ -2,53 +2,22 @@ import { tweetModel } from '../../models';
 
 const addBasicTweet = async (_: any, args: any) => {
   const userId = 'test1';
-  const content = '트윗 내용';
-  const imgUrls = [] as Array<String>;
+  const content = args.content;
+  const imgUrls = args.img_url_list;
   const newTweet = await tweetModel.create({
     author_id: userId,
     content: content,
     img_url_list: imgUrls,
     child_tweet_list: [],
   });
-
-  const newTweetId = newTweet?.get('_id');
-
-  const resultTweet = await tweetModel.aggregate([
-    {
-      $match: {
-        _id: newTweetId,
-      },
-    },
-    {
-      $project: {
-        author_id: 1,
-        content: 1,
-        img_url_list: 1,
-        parent_id: 1,
-        retweet_id: 1,
-        child_tweet_list: 1,
-        child_tweet_number: { $size: '$child_tweet_list' },
-      },
-    },
-    {
-      $lookup: {
-        from: 'users',
-        localField: 'author_id',
-        foreignField: 'user_id',
-        as: 'author',
-      },
-    },
-    { $unwind: '$author' },
-  ]);
-
-  return resultTweet[0];
+  return newTweet;
 };
 
 const addReplyTweet = async (_: any, args: any) => {
   const userId = 'test1';
-  const content = '트윗 내용';
-  const imgUrls = [] as Array<String>;
-  const parentId = '5fbca83023d2695f10ab52b4';
+  const content = args.content;
+  const imgUrls = args.img_url_list;
+  const parentId = args.parent_id;
 
   const replyTweet = await tweetModel.create({
     author_id: userId,
@@ -63,99 +32,21 @@ const addReplyTweet = async (_: any, args: any) => {
     { $push: { child_tweet_list: childId } },
     { new: true },
   );
-
-  const resultTweet = await tweetModel.aggregate([
-    {
-      $match: {
-        _id: childId,
-      },
-    },
-    {
-      $project: {
-        author_id: 1,
-        content: 1,
-        img_url_list: 1,
-        parent_id: 1,
-        retweet_id: 1,
-        child_tweet_list: 1,
-        child_tweet_number: { $size: '$child_tweet_list' },
-      },
-    },
-    {
-      $lookup: {
-        from: 'users',
-        localField: 'author_id',
-        foreignField: 'user_id',
-        as: 'author',
-      },
-    },
-    { $unwind: '$author' },
-  ]);
-
-  return resultTweet[0];
+  return replyTweet;
 };
 
 const addRetweet = async (_: any, args: any) => {
   const userId = 'test1';
-  const content = '트윗 리트윗';
-  const retweetId = '5fbca83023d2695f10ab52b4';
+  const content = args.content;
+  const retweetId = args.retweet_id;
 
-  const newTweet = await tweetModel.create({
+  const newRetweet = await tweetModel.create({
     author_id: userId,
     content: content,
     retweet_id: retweetId,
     child_tweet_list: [],
   });
-
-  const newTweetId = newTweet?.get('_id');
-
-  const resultTweet = await tweetModel.aggregate([
-    {
-      $match: {
-        _id: newTweetId,
-      },
-    },
-    {
-      $project: {
-        author_id: 1,
-        content: 1,
-        img_url_list: 1,
-        parent_id: 1,
-        retweet_id: 1,
-        child_tweet_list: 1,
-        child_tweet_number: { $size: '$child_tweet_list' },
-      },
-    },
-    {
-      $lookup: {
-        from: 'users',
-        localField: 'author_id',
-        foreignField: 'user_id',
-        as: 'author',
-      },
-    },
-    { $unwind: '$author' },
-    {
-      $lookup: {
-        from: 'tweets',
-        localField: 'retweet_id',
-        foreignField: '_id',
-        as: 'retweet',
-      },
-    },
-    { $unwind: '$retweet' },
-    {
-      $lookup: {
-        from: 'users',
-        localField: 'retweet.author_id',
-        foreignField: 'user_id',
-        as: 'retweet.author',
-      },
-    },
-    { $unwind: '$retweet.author' },
-  ]);
-
-  return resultTweet[0];
+  return newRetweet;
 };
 
 export { addBasicTweet, addReplyTweet, addRetweet };
