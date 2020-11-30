46a32ff84daa0263360f22bb70627bbe0abf720d
wseungjin <bose1021@naver.com>
2020-11-30 17:01:32 +0900

    [refac] makeStyles 지우고 스타일드 컴포넌트 사용

--- fe/src/components/atoms/Text/index.tsx
+++ fe/src/components/atoms/Text/index.tsx
@@ -1,56 +1,31 @@
 import React, { FunctionComponent } from 'react';
-import { Typography, makeStyles } from '@material-ui/core';
+import { StyledText, StyledTitleText, StyledSubText } from './styled';
 
 interface Props {
+  className: string;
   value: string;
   color?: string;
   size?: string;
   weight?: number;
-  styled?: string;
+  styled: 'root' | 'title' | 'sub';
 }
-
-interface StyledProps {
-  color: string;
-  size: string;
-  weight?: number;
-}
-
-const useStyles = makeStyles({
-  root: (styledProps: StyledProps) => ({
-    color: styledProps.color,
-    fontSize: styledProps.size,
-    fontWeight: styledProps.weight,
-  }),
-  title: {
-    color: '#000',
-    fontSize: '15px',
-    fontWeight: 700,
-    display: 'inline-block',
-  },
-  sub: {
-    color: 'rgb(91, 112, 131)',
-    fontSize: '15px',
-    fontWeight: 400,
-    display: 'inline-block',
-  },
-});
 
 const Text: FunctionComponent<Props> = ({
+  className,
   value,
   color = '#000',
   size = '15px',
   weight = 400,
-  styled = 'root',
+  styled = 'root'
 }) => {
-  const classes = useStyles({ color, size, weight });
   switch (styled) {
     case 'root':
     default:
-      return <Typography className={classes.root}>{value}</Typography>;
+      return <StyledText className={className} color= {color} theme = {{fontSize:size , fontWeight: weight}}>{value}</StyledText>;
     case 'title':
-      return <Typography className={classes.title}>{value}</Typography>;
+      return <StyledTitleText className={className}>{value}</StyledTitleText>;
     case 'sub':
-      return <Typography className={classes.sub}>{value}</Typography>;
+      return <StyledSubText className={className}>{value}</StyledSubText>;
   }
 };
 
