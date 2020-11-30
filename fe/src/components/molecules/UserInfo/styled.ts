d9b5c9dfbbfd7a64319e73a0fe2dcf0508f252c4
Baek Jiyoung <bjyoung0@gmail.com>
2020-11-30 21:45:46 +0900

    [refactor] user info에 사용되는 titlesub css 스타일 추가

--- fe/src/components/molecules/UserInfo/styled.ts
+++ fe/src/components/molecules/UserInfo/styled.ts
@@ -1,14 +1,24 @@
 import { Box } from '@material-ui/core';
 import styled from 'styled-components';
+import TitleSubText from '../TitleSubText';
 
 const Container = styled(Box)`
   border-radius: 15px;
   display: flex;
   align-items: center;
   cursor: pointer;
+
   &:hover {
     background-color: rgba(0, 0, 0, 0.04);
   }
 `;
 
-export default Container;
+const StyledTitleSub = styled(TitleSubText)`
+  margin-left: 5px;
+
+  & span {
+    display: block;
+  }
+`;
+
+export { Container, StyledTitleSub };
