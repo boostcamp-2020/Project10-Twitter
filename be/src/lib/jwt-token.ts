422357f5fafdefecbfb604c6a39238fe1099cbdf
DESKTOP-J5O26G9\admin <shmallow@naver.com>
2020-12-01 19:31:10 +0900

    [reafctor] token 생성 관련 로직 수정
    1. 기존에는 userData를 이용해 token을 만들었지만, 이렇게 만들경우 해당 유저의 Name이 변경되었을 경우 다시 Token을 만들어야하는 번거러움이 생김
    2. id만을 가지고 token을 만들도록 로직을 수정

--- be/src/lib/jwt-token.ts
+++ be/src/lib/jwt-token.ts
@@ -1,20 +1,17 @@
 import jwt from 'jsonwebtoken';
 
-const signToken = (user: any) => {
-  const userData = {
-    _id: user._id,
-    name: user.name,
-    user_id: user.user_id,
-    profile_img_url: user.profile_img_url,
-  };
-  const token = jwt.sign(userData, process.env.JWT_SECRET_KEY!, { expiresIn: '24h' });
+const signToken = ({ id }: { id: string }) => {
+  const token = jwt.sign({ id }, process.env.JWT_SECRET_KEY!, { expiresIn: '24h' });
   return token;
 };
 
 const verifyToken = (token: string) => {
-  if (!token) return null;
-  const user = jwt.verify(token, process.env.JWT_SECRET_KEY!);
-  return user;
+  try {
+    const user = jwt.verify(token, process.env.JWT_SECRET_KEY!);
+    return user;
+  } catch {
+    return null;
+  }
 };
 
 export { signToken, verifyToken };
