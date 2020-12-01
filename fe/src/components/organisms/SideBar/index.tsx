9b6fbdbd1b93cd6b58e545eff2180498d5cb9f29
wseungjin <bose1021@naver.com>
2020-12-02 00:07:33 +0900

    [feat] 프로필 클릭시 내 프로필 아이디로 유저 검색

--- fe/src/components/organisms/SideBar/index.tsx
+++ fe/src/components/organisms/SideBar/index.tsx
@@ -10,6 +10,7 @@ import GET_MYINFO from '../../../graphql/getMyInfo.gql';
 import Container from './styled';
 import useOnTextChange from '../../../hooks/useOnTextChange';
 import useDisplay from '../../../hooks/useDisplay';
+import Link from 'next/link';
 
 interface ButtonProps {
   id: number;
@@ -18,42 +19,49 @@ interface ButtonProps {
   color?: 'primary' | 'inherit' | 'default' | 'secondary' | undefined;
   variant?: 'contained' | 'text' | 'outlined' | undefined;
   width?: string;
-  onClick?: () => void;
+  link: string;
 }
 
 const TITLE: Array<ButtonProps> = [
-  { id: 0, text: '', icon: Twitter({ width: '40px', height: '40px' }) },
-  { id: 1, text: '홈', icon: Home({ width: '30px', height: '30px' }) },
-  { id: 2, text: '탐색하기', icon: Explore({ width: '30px', height: '30px' }) },
-  { id: 3, text: '알림', icon: Notifications({ width: '30px', height: '30px' }) },
-  { id: 4, text: '프로필', icon: Profiles({ width: '30px', height: '30px' }) },
+  { id: 0, text: '', icon: Twitter({ width: '40px', height: '40px'}),link:'/home' },
+  { id: 1, text: '홈', icon: Home({ width: '30px', height: '30px' }),link:'/home' },
+  { id: 2, text: '탐색하기', icon: Explore({ width: '30px', height: '30px' }),link:'/home' },
+  { id: 3, text: '알림', icon: Notifications({ width: '30px', height: '30px' }) ,link:'/home'},
+  { id: 4, text: '프로필', icon: Profiles({ width: '30px', height: '30px' }),link:'/'},
   {
     id: 5,
     text: 'Tweet',
     color: 'primary',
     variant: 'contained',
     width: '90%',
+    link:'/home'
   },
 ];
 
 const SideBar: FunctionComponent = () => {
-  const { data } = useQuery(GET_MYINFO);
+  const { loading, error, data } = useQuery(GET_MYINFO);
   const placeholder = 'Search Twitter';
   const type = 'text';
   const variant = 'standard';
 
   const [value,, onTextChange] = useOnTextChange('')
   const [display,, onClick] = useDisplay(false)
-
   
-  if (data) {
-    const userId = data.user.user_id;
-    const userName = data.user.name;
-    const userProfileImg = data.user.profile_img_url;
+  if (loading) return <div>'Loading...'</div>;
+  if (error) return <div>`Error! ${error.message}`</div>;
+  
+  const {myProfile} = data
+    
+  const userId:string = myProfile.user_id;
+  const userName:string = myProfile.name;
+  const userProfileImg:string = myProfile.profile_img_url;
+  TITLE[4].link = userId;
+
   return (
     <Container component="ul">
       {TITLE.map((v) => (
         <ListItem key={v.id}>
+          <Link href={v.link}>
           <Button
             text={v.text}
             icon={v.icon}
@@ -61,6 +69,7 @@ const SideBar: FunctionComponent = () => {
             variant={v.variant}
             width={v.width}
           />
+          </Link>
         </ListItem>
       ))}
           <ListItem>
@@ -76,8 +85,8 @@ const SideBar: FunctionComponent = () => {
           {display ? <UserModal /> : <></>}
           <ListItem onClick={onClick}>
             <UserInfo
-              title={userId}
-              sub={userName}
+              title={userName}
+              sub={userId}
               img={userProfileImg}
               width="90%"
             />
@@ -86,8 +95,6 @@ const SideBar: FunctionComponent = () => {
           </Container>
 
   );
-      }
-      return <div>loading...</div>;
-    };
+};
 
 export default SideBar;
