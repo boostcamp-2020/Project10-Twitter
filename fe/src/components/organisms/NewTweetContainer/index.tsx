6bcf300cf67f0389d54fd3edad5ccb92b3907648
wseungjin <bose1021@naver.com>
2020-12-01 15:23:56 +0900

    [refac] 뉴 트윗 컨테이너 커스텀 훅으로 리팩토링

--- fe/src/components/organisms/NewTweetContainer/index.tsx
+++ fe/src/components/organisms/NewTweetContainer/index.tsx
@@ -1,36 +1,37 @@
 import React, { FunctionComponent, ReactElement, useState } from 'react';
+import { useQuery } from '@apollo/client';
 import MainContaier from '../MainContainer';
 import TextArea from '../../atoms/TextArea';
 import IconButton from '../../molecules/IconButton';
 import { Picture } from '../../atoms/Icons';
 import Button from '../../molecules/Button';
 import ButtonsBox from './styled';
+import GET_MYINFO from '../../../graphql/getMyInfo.gql';
+import useOnTextChange from '../../../hooks/useOnTextChange';
 
-interface Props {
-  img?: string;
-}
+const NewTweetContainer: FunctionComponent= () => {
+  const { data } = useQuery(GET_MYINFO);
+  const [value,,onTextChange] = useOnTextChange('')
 
-const NewTweetContainer: FunctionComponent<Props> = ({ img = '' }) => {
   const placeholder = "What's happening";
   const content = 'Tweet';
   const color = 'primary';
   const variant = 'contained';
   const borderRadius = 50;
 
-  const [value, setValue] = useState('');
-  const onChange = (e: React.SyntheticEvent) => {
-    const target = e.target as HTMLInputElement;
-    setValue(target.value);
-  };
 
-  return (
-    <MainContaier ProfileImgUrl={img}>
-      <TextArea placeholder={placeholder} value={value} onChange={onChange} />
-      <ButtonsBox component="div">
-        <IconButton icon={Picture} />
-        <Button borderRadius={borderRadius} text={content} color={color} variant={variant} />
-      </ButtonsBox>
-    </MainContaier>
-  );
+  if (data) {
+    const userProfileImg = data.user.profile_img_url;
+    return (
+      <MainContaier ProfileImgUrl={userProfileImg}>
+        <TextArea placeholder={placeholder} value={value} onChange={onTextChange} />
+        <ButtonsBox component="div">
+          <IconButton icon={Picture} />
+          <Button borderRadius={borderRadius} text={content} color={color} variant={variant} />
+        </ButtonsBox>
+      </MainContaier>
+    );
+  }
+  return <div>loading...</div>;
 };
 export default NewTweetContainer;
