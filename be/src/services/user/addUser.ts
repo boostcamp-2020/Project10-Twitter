a4ff4462596fc134f02a9f04fe2bc136163148d8
DESKTOP-J5O26G9\admin <shmallow@naver.com>
2020-12-01 19:18:01 +0900

    [feat] 로컬 회원가입 API 작성

--- be/src/services/user/addUser.ts
+++ be/src/services/user/addUser.ts
@@ -1,2 +1,33 @@
 import { userModel } from '../../models';
-import { AuthenticationError } from 'apollo-server-express';
+import getHashedPassword from '../../lib/hash-password';
+
+interface UserInfo {
+  user_id: string;
+  name: string;
+  password?: string;
+  profile_img_url?: string;
+}
+
+const registerUser = async (userInfo: UserInfo) => {
+  const user = await userModel.create({
+    user_id: userInfo.user_id,
+    name: userInfo.name,
+    password: userInfo.password,
+    following_list: [],
+    profile_img_url: userInfo.profile_img_url,
+    heart_tweet_list: [],
+  });
+  return user;
+};
+
+const createUser = async (_: any, args: UserInfo) => {
+  if (!args.user_id || !args.password || !args.name) {
+    throw Error('Not enough information');
+  }
+  args.password = await getHashedPassword(args.password);
+  await registerUser(args);
+
+  return { response: true };
+};
+
+export { registerUser, createUser };
