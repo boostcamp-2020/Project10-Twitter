2ec8745820aaeadb423c16008f029b3060f7126b
wseungjin <bose1021@naver.com>
2020-12-02 17:10:50 +0900

    [feat] 유저 프로필, 배경 사진 , 자기소개 수정 API

--- be/src/services/user/modifyUser.ts
+++ be/src/services/user/modifyUser.ts
@@ -6,44 +6,88 @@ interface Auth {
   authUser: { user_id: string };
 }
 
-const followUser = async (_: any, args: { follow_user_id: String }, { authUser }: Auth) => {
+const followUser = async (
+  _: any,
+  { follow_user_id }: { follow_user_id: String },
+  { authUser }: Auth,
+) => {
   if (!authUser) throw new AuthenticationError('not authenticated');
 
   const userId = authUser.user_id;
-  const followUserId = args.follow_user_id;
 
-  if (followUserId === userId) throw new Error('not allow follow yourself');
+  if (follow_user_id === userId) throw new Error('not allow follow yourself');
 
   const user = await userModel.findOneAndUpdate(
     { user_id: userId },
-    { $addToSet: { following_list: followUserId } },
+    { $addToSet: { following_list: follow_user_id } },
     { new: true },
   );
-  await createNotifiaction({ userId: followUserId, followerId: userId, type: 'follow' });
+  await createNotifiaction({ userId: follow_user_id, followerId: userId, type: 'follow' });
   return user;
 };
 
-const unfollowUser = async (_: any, args: { unfollow_user_id: String }, { authUser }: Auth) => {
+const unfollowUser = async (
+  _: any,
+  { unfollow_user_id }: { unfollow_user_id: String },
+  { authUser }: Auth,
+) => {
   if (!authUser) throw new AuthenticationError('not authenticated');
 
   const userId = authUser.user_id;
-  const unfollowUserId = args.unfollow_user_id;
 
   const user = await userModel.findOneAndUpdate(
     { user_id: userId },
-    { $pull: { following_list: unfollowUserId } },
+    { $pull: { following_list: unfollow_user_id } },
     { new: true },
   );
   return user;
 };
 
-const updateUserName = async (_: any, args: { name: String }, { authUser }: Auth) => {
+const updateUserName = async (_: any, { name }: { name: String }, { authUser }: Auth) => {
   if (!authUser) throw new AuthenticationError('not authenticated');
 
   const userId = authUser.user_id;
-  const { name } = args;
   await userModel.update({ user_id: userId }, { $set: { name } });
   return { response: true };
 };
 
-export { followUser, unfollowUser, updateUserName };
+const updateUserComment = async (_: any, { comment }: { comment: String }, { authUser }: Auth) => {
+  if (!authUser) throw new AuthenticationError('not authenticated');
+
+  const userId = authUser.user_id;
+  await userModel.update({ user_id: userId }, { $set: { comment } });
+  return { response: true };
+};
+
+const updateUserProfileImg = async (
+  _: any,
+  { profile_img_url }: { profile_img_url: String },
+  { authUser }: Auth,
+) => {
+  if (!authUser) throw new AuthenticationError('not authenticated');
+
+  const userId = authUser.user_id;
+  await userModel.update({ user_id: userId }, { $set: { profile_img_url } });
+  return { response: true };
+};
+
+const updateUserBackgroundImg = async (
+  _: any,
+  { background_img_url }: { background_img_url: String },
+  { authUser }: Auth,
+) => {
+  if (!authUser) throw new AuthenticationError('not authenticated');
+
+  const userId = authUser.user_id;
+  await userModel.update({ user_id: userId }, { $set: { background_img_url } });
+  return { response: true };
+};
+
+export {
+  followUser,
+  unfollowUser,
+  updateUserName,
+  updateUserComment,
+  updateUserProfileImg,
+  updateUserBackgroundImg,
+};
