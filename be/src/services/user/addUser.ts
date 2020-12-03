6cb3a4fd19f6f0380cc2d23dd226f47413402173
DESKTOP-J5O26G9\admin <shmallow@naver.com>
2020-12-03 16:31:53 +0900

    [refactor] GitHub 로그인할 경우, 아이디가 없으면 새로운 아이디를 만들도록 구현하는데, 해당 아이디가 이미 존재하는 경우 랜덤 4자리 문자열을 붙여서 유니크한 값으로 만든 다음 저장하도록 수정

--- be/src/services/user/addUser.ts
+++ be/src/services/user/addUser.ts
@@ -1,17 +1,33 @@
 import { userModel } from '../../models';
 import getHashedPassword from '../../lib/hash-password';
+import { makeRandomName } from '../../lib/utilty';
 
 interface UserInfo {
   user_id: string;
   name: string;
+  github_id: number;
   password?: string;
   profile_img_url?: string;
 }
 
+const checkUnique = async (userId: string): Promise<Boolean> => {
+  const user = await userModel.findOne({ user_id: userId });
+  if (!user) return true;
+  return false;
+};
+
+const makeUniqueUserId = async (userId: string): Promise<string> => {
+  const isUnique = await checkUnique(userId);
+  if (isUnique) return userId;
+  return makeUniqueUserId(userId + '_' + makeRandomName(4));
+};
+
 const registerUser = async (userInfo: UserInfo) => {
+  const uniqueUserId = await makeUniqueUserId(userInfo.user_id);
   const user = await userModel.create({
-    user_id: userInfo.user_id,
-    name: userInfo.name,
+    user_id: uniqueUserId,
+    github_id: userInfo.github_id,
+    name: userInfo.name ? userInfo.name : makeRandomName(16),
     password: userInfo.password,
     following_id_list: [],
     profile_img_url: userInfo.profile_img_url,
