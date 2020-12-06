a10b2dde4503502dd428619330b8de13f923ca8e
wseungjin <bose1021@naver.com>
2020-12-06 20:57:09 +0900

    [feat] 유저 디테일에서 팔로우 및 언팔로우 가능

--- fe/src/components/organisms/UserDetailContainer/index.tsx
+++ fe/src/components/organisms/UserDetailContainer/index.tsx
@@ -1,6 +1,7 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState, useEffect } from 'react';
 import Link from 'next/link';
-import { useQuery } from '@apollo/client';
+import { useQuery, useMutation } from '@apollo/client';
+import useUserState from '../../../hooks/useUserState';
 import TitleSubText from '../../molecules/TitleSubText';
 import ProfileImg from '../../atoms/ProfileImg';
 import Button from '../../molecules/Button';
@@ -16,8 +17,10 @@ import {
   ImgCircleContainer,
 } from './styled';
 import Text from '../../atoms/Text';
+import { getJSXwithUserState } from '../../../utilitys';
 import GET_USERDETAIL from '../../../graphql/getUserDetail.gql';
-import GET_MYINFO from '../../../graphql/getMyInfo.gql';
+import FOLLOW_USER from '../../../graphql/followUser.gql';
+import UNFOLLOW_USER from '../../../graphql/unfollowUser.gql';
 
 interface Props {
   userId: string;
@@ -34,9 +37,28 @@ interface Variable {
 const UserDetailContainer: FunctionComponent<Props> = ({ children, userId }) => {
   const queryVariable: QueryVariable = { variables: { userId: userId as string } };
   const { loading, error, data } = useQuery(GET_USERDETAIL, queryVariable);
-  const { loading: myInfoLoading, error: myInfoError, data: myInfo } = useQuery(GET_MYINFO);
+  const [userValue, setUserValue] = useState({ user_id: 'initial', following_id_list: [''] });
+  const [userState, setFollowUser, setUnfollowUser] = useUserState(userValue);
 
-  const PROFILE_IMG_SIZE = 150;
+  useEffect(() => {
+    if (data) setUserValue(data.user);
+  }, [data]);
+
+  const [followUser] = useMutation(FOLLOW_USER);
+
+  const [unfollowUser] = useMutation(UNFOLLOW_USER);
+
+  const onClickEdit = () => {};
+
+  const onClickFollow = () => {
+    followUser({ variables: { follow_user_id: user.user_id } });
+    setFollowUser();
+  };
+
+  const onClickUnfollow = () => {
+    unfollowUser({ variables: { unfollow_user_id: user.user_id } });
+    setUnfollowUser();
+  };
 
   if (loading) return <div>Loading...</div>;
   if (error)
@@ -47,38 +69,44 @@ const UserDetailContainer: FunctionComponent<Props> = ({ children, userId }) => 
       </div>
     );
 
-  const { userDetail, followingList, followerList } = data;
+  const PROFILE_IMG_SIZE = 150;
+
+  const { user, followingList, followerList } = data;
 
   return (
     <DetailContainer>
       <UserBackgroundContainer>
-        <img src={userDetail.background_img_url} alt="user background" />
+        <img src={user.background_img_url} alt="user background" />
       </UserBackgroundContainer>
       <UserMainContainer>
         <TopContainer>
           <UserImgContainer>
             <ImgCircleContainer>
-              <ProfileImg img={userDetail.profile_img_url} size={PROFILE_IMG_SIZE} />
+              <ProfileImg img={user.profile_img_url} size={PROFILE_IMG_SIZE} />
             </ImgCircleContainer>
           </UserImgContainer>
-          {myInfo.myProfile.following_id_list.includes(userDetail.user_id) ? (
-            <Button text="unfollow" color="primary" variant="contained" />
-          ) : myInfo.myProfile.user_id != userDetail.user_id ? (
-            <Button text="follow" color="primary" variant="outlined" />
-          ) : (
-            <Button text="edit" variant="outlined" color="primary" />
+          {getJSXwithUserState(
+            userState,
+            <Button text="edit" variant="outlined" color="primary" onClick={onClickEdit} />,
+            <Button
+              text="unfollow"
+              color="primary"
+              variant="contained"
+              onClick={onClickUnfollow}
+            />,
+            <Button text="follow" color="primary" variant="outlined" onClick={onClickFollow} />,
           )}
         </TopContainer>
         <BottomContainer>
-          <TitleSubText title={userDetail.name} sub={userDetail.user_id} />
-          <Text value={userDetail.comment} />
+          <TitleSubText title={user.name} sub={user.user_id} />
+          <Text value={user.comment} />
           <UserFollowContainer>
-            <Link href={`/${userDetail.user_id}/follow?Following`}>
+            <Link href={`/${user.user_id}/follow?Following`}>
               <a>
                 <TitleSubText title="팔로잉 수" sub={followingList.length} />
               </a>
             </Link>
-            <Link href={`/${userDetail.user_id}/follow?Follower`}>
+            <Link href={`/${user.user_id}/follow?Follower`}>
               <a>
                 <TitleSubText title="팔로워 수" sub={followerList.length} />
               </a>
