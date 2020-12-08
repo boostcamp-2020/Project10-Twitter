6d48add7c144910801faa6a6c5149603d708254e
Jae-Woo Joo <wnwodn4293@naver.com>
2020-12-08 18:14:17 +0900

    [feat] 탐색 동적 라우팅 구현
    - tab 상태 사용하지 않고 url 의 쿼리값으로 대체
    - 동적라우팅 구현
    - tab 클릭 이벤트에 shallow 라우팅 구현

--- fe/src/pages/explore/[[...type]].tsx
+++ fe/src/pages/explore/[[...type]].tsx
@@ -1,17 +1,18 @@
+/* eslint-disable no-nested-ternary */
 /* eslint-disable camelcase */
 /* eslint-disable react/no-array-index-key */
 import React, { FunctionComponent, useState, useEffect } from 'react';
 import { useQuery } from '@apollo/client';
 import { useRouter } from 'next/router';
-import SearchBar from '../../components/molecules/SearchBar'
+import SearchBar from '../../components/molecules/SearchBar';
 import SideBar from '../../components/organisms/SideBar';
 import TabBar from '../../components/molecules/TabBar';
 import { Container, MainContainer } from './styled';
 import TweetContainer from '../../components/organisms/TweetContainer';
 import UserCard from '../../components/organisms/UserCard';
 import useOnTabChange from '../../hooks/useOnTabChange';
-import GET_SEARCH_TWEETLIST from '../../graphql/getSearchedTweetList.gql'
-import GET_SEARCH_USERLIST from '../../graphql/getSearchedUserList.gql'
+import GET_SEARCH_TWEETLIST from '../../graphql/getSearchedTweetList.gql';
+import GET_SEARCH_USERLIST from '../../graphql/getSearchedUserList.gql';
 import useOnTextChange from '../../hooks/useOnTextChange';
 
 interface QueryVariable {
@@ -23,6 +24,7 @@ interface Variable {
 }
 
 interface Tweet {
+  _id: string;
   content: string;
   author: Author;
   child_tweet_number: number;
@@ -44,22 +46,34 @@ interface Author {
 
 const Explore: FunctionComponent = () => {
   const router = useRouter();
-  const param = router.asPath.split('?')[2] || 'tweets';
-  const splitedSearchWord = router.asPath.split('?')[1]
-  const initalText = splitedSearchWord ? splitedSearchWord.split("=")[1] : '';
-  const [textValue,setTextValue,onTextChange] = useOnTextChange(initalText); 
-  const [value, , onChange] = useOnTabChange(param);
-  const queryArr = { tweets: GET_SEARCH_TWEETLIST, 'people': GET_SEARCH_USERLIST };
+  const { type } = router.query;
+  const [textValue, setTextValue, onTextChange] = useOnTextChange(type ? type[1] || '' : '');
+  const value = type ? type[0] : 'tweets';
+  const queryArr = { tweets: GET_SEARCH_TWEETLIST, people: GET_SEARCH_USERLIST };
   const queryVariable: QueryVariable = { variables: { searchWord: textValue } };
   const { loading, error, data, refetch } = useQuery(queryArr[value], queryVariable);
 
   const placeholder = 'Search Twitter';
-  const type = 'text';
   const variant = 'standard';
 
   useEffect(() => {
-    router.replace(`/explore`, `/explore?searchWord=${textValue}?${value}`, { shallow: true });
-  }, [value,textValue]);
+    router.replace(
+      `/explore/[[...type]]`,
+      `/explore/${value !== '' ? `${value}/` : ''}${textValue}`,
+      {
+        shallow: true,
+      },
+    );
+  }, [textValue]);
+
+  const onClick = (e: React.SyntheticEvent<EventTarget>) => {
+    const target = e.target as HTMLInputElement;
+    let newValue = target.textContent;
+    if (newValue !== value) {
+      if (newValue === 'tweets') newValue = '';
+      router.replace('/explore/[[...type]]', `/explore/${newValue}`, { shallow: true });
+    }
+  };
 
   return (
     <Container>
@@ -67,24 +81,24 @@ const Explore: FunctionComponent = () => {
       <MainContainer>
         <SearchBar
           placeholder={placeholder}
-          type={type}
+          type="text"
           variant={variant}
           width="90%"
           value={textValue}
           onChange={onTextChange}
         />
-        <TabBar value={value} handleChange={onChange} labels={['tweets', 'people']} />
+        <TabBar value={value} handleChange={onClick} labels={['tweets', 'people']} />
         {data ? (
-        data.tweetList ? (
-          data.tweetList?.map((tweet: Tweet, index: number) => (
-            <TweetContainer key={index} tweet={tweet} />
-          ))
+          data.tweetList ? (
+            data.tweetList?.map((tweet: Tweet, index: number) => (
+              <TweetContainer key={index} tweet={tweet} />
+            ))
+          ) : (
+            data.userList?.map((user: User, index: number) => <UserCard key={index} user={user} />)
+          )
         ) : (
-          data.userList?.map((user: User, index: number) => (
-            <UserCard key={index} user={user} />
-          )))
-        ): (<div> loading..</div>)
-        }
+          <div> loading..</div>
+        )}
       </MainContainer>
     </Container>
   );
