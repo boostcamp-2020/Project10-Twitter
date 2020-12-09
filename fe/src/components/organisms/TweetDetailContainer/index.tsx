8c79476ffa3562f733686c3fb4102683369f8362
Baek Jiyoung <bjyoung0@gmail.com>
2020-12-10 00:13:59 +0900

    [feat] TweetDetailContainer 해당하는 Modal 나오도록 변경 - heart를 누른 사람이 있을 경우 해당 정보를 보여주는 버튼 구현 - retweet을 한 사람이 있을 경우 해당 정보를 보여주는 버튼 구현 - 둘 다 없을 경우 text를 보여줌 - 해당 버튼 클릭 시 알맞는 modal 나오도록 구현

--- fe/src/components/organisms/TweetDetailContainer/index.tsx
+++ fe/src/components/organisms/TweetDetailContainer/index.tsx
@@ -1,10 +1,10 @@
 import React, { FunctionComponent, useState, useEffect } from 'react';
 import Link from 'next/link';
+import Markdown from 'react-markdown/with-html';
 import { useQuery } from '@apollo/client';
 import useHeartState from '../../../hooks/useHeartState';
 import TitleSubText from '../../molecules/TitleSubText';
 import IconButton from '../../molecules/IconButton';
-import Markdown from 'react-markdown/with-html';
 import {
   DetailContainer,
   TweetDetailInfoContainer,
@@ -14,6 +14,9 @@ import {
 import { Heart, Comment, Retweet } from '../../atoms/Icons';
 import GET_TWEET_DETAIL from '../../../graphql/getTweetDetail.gql';
 import UserInfo from '../../molecules/UserInfo';
+import useDisplay from '../../../hooks/useDisplay';
+import { HeartListModal, RetweetListModal, ReplyModal, RetweetModal } from '../TweetModal';
+import Text from '../../atoms/Text';
 
 interface Props {
   tweetId: string;
@@ -27,10 +30,14 @@ interface Variable {
   tweetId: string;
 }
 
-const UserDetailContainer: FunctionComponent<Props> = ({ children, tweetId }) => {
+const TweetDetailContainer: FunctionComponent<Props> = ({ children, tweetId }) => {
   const queryVariable: QueryVariable = { variables: { tweetId: tweetId as string } };
   const { loading, error, data } = useQuery(GET_TWEET_DETAIL, queryVariable);
   const [isHeart, onClickHeart, onClickUnheart] = useHeartState(data?.tweet);
+  const [displayReplyModal, , onClickReplyBtn] = useDisplay(false);
+  const [displayRetweetModal, , onClickRetweetBtn] = useDisplay(false);
+  const [displayHeartListModal, , onClickHeartList] = useDisplay(false);
+  const [displayRetweetListModal, , onClickRetweetList] = useDisplay(false);
 
   if (loading) return <div>Loading...</div>;
   if (error)
@@ -44,38 +51,79 @@ const UserDetailContainer: FunctionComponent<Props> = ({ children, tweetId }) =>
   const { tweet } = data;
 
   return (
-    <DetailContainer>
-      <Link href={`/${tweet.author.user_id}`}>
-        <UserInfo
-          img={tweet.author.profile_img_url}
-          title={tweet.author.name}
-          sub={tweet.author.user_id}
-        />
-      </Link>
-      <Markdown allowDangerousHtml>{tweet.content}</Markdown>
-      <TweetDetailInfoContainer>
-        <Link href={`/status/${tweetId}/retweets`}>
-          <a>
-            <TitleSubText title={tweet.retweet_user_number} sub={'Retweets'} />
-          </a>
+    <>
+      <DetailContainer>
+        <Link href={`/${tweet.author.user_id}`}>
+          <UserInfo
+            img={tweet.author.profile_img_url}
+            title={tweet.author.name}
+            sub={tweet.author.user_id}
+          />
         </Link>
-        <Link href={`/status/${tweetId}/likes`}>
-          <a>
-            <TitleSubText title={tweet.heart_user_number} sub={'Likes'} />
-          </a>
-        </Link>
-      </TweetDetailInfoContainer>
-      <ButtonsContainer>
-        <IconButton icon={Comment} />
-        <IconButton icon={Retweet} />
-        {isHeart ? (
-          <PinkIconButton icon={Heart} onClick={onClickUnheart} />
-        ) : (
-          <IconButton icon={Heart} onClick={onClickHeart} />
-        )}
-      </ButtonsContainer>
-    </DetailContainer>
+        <Markdown allowDangerousHtml>{tweet.content}</Markdown>
+        <TweetDetailInfoContainer>
+          {/* <Link href={`/status/${tweetId}/retweets`}>
+            <a>
+              <TitleSubText title={tweet.retweet_user_number} sub="Retweets" />
+            </a>
+          </Link>
+          <Link href={`/status/${tweetId}/likes`}>
+            <a>
+              <TitleSubText title={tweet.heart_user_number} sub="Likes" />
+            </a>
+          </Link> */}
+          {tweet.retweet_user_number > 0 ? (
+            <TitleSubText
+              title={tweet.retweet_user_number}
+              sub="Retweets"
+              onClick={onClickRetweetList}
+            />
+          ) : (
+            <></>
+          )}
+          {tweet.heart_user_number > 0 ? (
+            <TitleSubText title={tweet.heart_user_number} sub="Likes" onClick={onClickHeartList} />
+          ) : (
+            <></>
+          )}
+          {tweet.retweet_user_number <= 0 && tweet.retweet_user_number <= 0 ? (
+            <Text value="View Tweet Activity" styled="sub" />
+          ) : (
+            <></>
+          )}
+        </TweetDetailInfoContainer>
+        <ButtonsContainer>
+          <IconButton icon={Comment} onClick={onClickReplyBtn} />
+          <IconButton icon={Retweet} onClick={onClickRetweetBtn} />
+          {isHeart ? (
+            <PinkIconButton icon={Heart} onClick={onClickUnheart} />
+          ) : (
+            <IconButton icon={Heart} onClick={onClickHeart} />
+          )}
+        </ButtonsContainer>
+      </DetailContainer>
+      <ReplyModal
+        displayModal={displayReplyModal}
+        onClickCloseBtn={onClickReplyBtn}
+        tweet={tweet}
+      />
+      <RetweetModal
+        displayModal={displayRetweetModal}
+        onClickCloseBtn={onClickRetweetBtn}
+        tweet={tweet}
+      />
+      <HeartListModal
+        displayModal={displayHeartListModal}
+        onClickCloseBtn={onClickHeartList}
+        tweetId={tweet._id}
+      />
+      <RetweetListModal
+        displayModal={displayRetweetListModal}
+        onClickCloseBtn={onClickRetweetList}
+        tweetId={tweet._id}
+      />
+    </>
   );
 };
 
-export default UserDetailContainer;
+export default TweetDetailContainer;
