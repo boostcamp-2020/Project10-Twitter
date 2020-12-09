dd80bbbd0400238a3e5fb923ea2bc1188490283d
Baek Jiyoung <bjyoung0@gmail.com>
2020-12-10 00:05:17 +0900

    [feat] retweet한 사람들을 볼 수 있는 modal 작성

---
+++ fe/src/components/organisms/TweetModal/RetweetListModal.tsx
@@ -0,0 +1,35 @@
+import React, { FunctionComponent } from 'react';
+import { useQuery } from '@apollo/client';
+import Modal from '../../molecules/Modal';
+import UserCard from '../UserCard';
+import GET_RETWEET_USER_LIST from '../../../graphql/getRetweetUserList.gql';
+
+interface Props {
+  displayModal: boolean;
+  onClickCloseBtn: () => void;
+  tweetId: string;
+}
+
+interface User {
+  user_id: string;
+  name: string;
+  profile_img_url?: string;
+  comment?: string;
+  following_user?: User;
+}
+
+const HeartListModal: FunctionComponent<Props> = ({ displayModal, onClickCloseBtn, tweetId }) => {
+  const { loading, error, data } = useQuery(GET_RETWEET_USER_LIST, { variables: { tweetId } });
+
+  return (
+    <Modal displayModal={displayModal} onClickCloseBtn={onClickCloseBtn}>
+      {data ? (
+        data.userList?.map((user: User, index: number) => <UserCard key={index} user={user} />)
+      ) : (
+        <>loading..</>
+      )}
+    </Modal>
+  );
+};
+
+export default HeartListModal;
