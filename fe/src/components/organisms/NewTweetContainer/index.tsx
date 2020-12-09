0250ac8fb965ab6ba6e2ac09101cfe216b390104
Baek Jiyoung <bjyoung0@gmail.com>
2020-12-09 23:51:17 +0900

    [feat] new tweet container 속성 replyTweet, basicTweet, retweet으로 나눔 - query를 인자로 받음 - reply tweet은 parentId로 구분 - retweet은 retweet으로 구분 - 기본 속성만 있는 경우 basic tweet으로 구분

--- fe/src/components/organisms/NewTweetContainer/index.tsx
+++ fe/src/components/organisms/NewTweetContainer/index.tsx
@@ -1,31 +1,49 @@
 import React, { FunctionComponent, useState, useEffect, ReactChild } from 'react';
-import { useMutation } from '@apollo/client';
+import { FetchResult, MutationFunctionOptions } from '@apollo/client';
 import useMyInfo from '../../../hooks/useMyInfo';
 import MainContaier from '../MainContainer';
+import ReTweetContaier from '../ReTweetContainer';
 import TextArea from '../../atoms/TextArea';
 import { Picture } from '../../atoms/Icons';
 import TweetFooter from '../../molecules/TweetFooter';
-import ADD_BASIC_TWEET from '../../../graphql/addBasicTweet.gql';
 import useOnTextChange from '../../../hooks/useOnTextChange';
-import Container from './styled';
 
 interface Props {
   children?: ReactChild;
+  tweet?: Tweet;
+  onClickQuery: (
+    options?: MutationFunctionOptions<any, Record<string, any>> | undefined,
+  ) => Promise<FetchResult<any, Record<string, any>, Record<string, any>>>;
+  parentId?: string;
 }
 
-const NewTweetContainer: FunctionComponent<Props> = () => {
+interface Tweet {
+  _id: string;
+  content: string;
+  child_tweet_number: number;
+  retweet_user_number: number;
+  heart_user_number: number;
+  author: Author;
+}
+interface Author {
+  user_id: string;
+  name: string;
+  profile_img_url: string;
+}
+
+const NewTweetContainer: FunctionComponent<Props> = ({ tweet, onClickQuery, parentId }) => {
   const { myProfile } = useMyInfo();
   const [value, setValue, onTextChange] = useOnTextChange('');
   const [btnDisabled, setBtnDisabled] = useState(true);
-  const [addBasicTweet, { loading: mutationLoading, error: mutationError }] = useMutation(
-    ADD_BASIC_TWEET,
-  );
+
   useEffect(() => {
     setBtnDisabled(!value);
   }, [value]);
 
   const onTweetBtnClick = () => {
-    addBasicTweet({ variables: { content: value } });
+    if (parentId) onClickQuery({ variables: { content: value, parentId } });
+    else if (tweet) onClickQuery({ variables: { content: value, retweetId: tweet._id } });
+    else onClickQuery({ variables: { content: value } });
     setValue('');
   };
 
@@ -34,6 +52,7 @@ const NewTweetContainer: FunctionComponent<Props> = () => {
   return (
     <MainContaier userId={myProfile.user_id} ProfileImgUrl={myProfile.profile_img_url}>
       <TextArea placeholder={placeholder} value={value} onChange={onTextChange} />
+      {tweet ? <ReTweetContaier tweet={tweet} /> : <></>}
       <TweetFooter onClick={onTweetBtnClick} btnDisabled={btnDisabled} icons={[Picture]} />
     </MainContaier>
   );
