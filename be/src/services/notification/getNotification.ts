a1259b8db5ed65a36af1745eda604786bedf8561
Jae-Woo Joo <wnwodn4293@naver.com>
2020-12-09 20:36:18 +0900

    [feat] 멘션 알림 받는 API 구현
    - 멘션 알림 받는 기능 구현
    - 리졸버, 스키마 작성

--- be/src/services/notification/getNotification.ts
+++ be/src/services/notification/getNotification.ts
@@ -10,7 +10,11 @@ const getNextnotificationsCondition = (oldest_notification_id: string): Object =
   return oldest_notification_id ? { _id: { $lt: stringToObjectId(oldest_notification_id) } } : {};
 };
 
-const getNotification = async (_: any, { oldest_notification_id }: any, { authUser }: Auth) => {
+const getNotification = async (
+  _: any,
+  { oldest_notification_id }: { oldest_notification_id: string },
+  { authUser }: Auth,
+) => {
   if (!authUser) throw new AuthenticationError('not authenticated');
 
   const userId = authUser.id;
@@ -23,6 +27,8 @@ const getNotification = async (_: any, { oldest_notification_id }: any, { authUs
         $and: [{ user_id: userId }, nextNotificationcondition],
       },
     },
+    { $sort: { createAt: -1 } },
+    { $limit: 20 },
     {
       $project: {
         tweet_id: 1,
@@ -59,21 +65,85 @@ const getNotification = async (_: any, { oldest_notification_id }: any, { authUs
       },
     },
     { $unwind: { path: '$user', preserveNullAndEmptyArrays: true } },
-    { $sort: { createAt: -1 } },
-    { $limit: 20 },
   ]);
 
   return notifiactions;
 };
 
-const getNotificationCount = async (_: any, __: any, { authUser }: Auth) => {
+const getNotificationWithMention = async (
+  _: any,
+  { oldest_notification_id }: { oldest_notification_id: string },
+  { authUser }: Auth,
+) => {
   if (!authUser) throw new AuthenticationError('not authenticated');
 
   const userId = authUser.id;
+
+  const nextNotificationcondition = getNextnotificationsCondition(oldest_notification_id);
+
+  const notifiactions: Document[] = await notificationModel.aggregate([
+    {
+      $match: {
+        $and: [{ user_id: userId }, { type: 'mention' }, nextNotificationcondition],
+      },
+    },
+    { $sort: { createAt: -1 } },
+    { $limit: 20 },
+    {
+      $project: {
+        tweet_id: 1,
+        follower_id: 1,
+        type: 1,
+        is_read: 1,
+        createAt: 1,
+      },
+    },
+    {
+      $lookup: {
+        from: 'tweets',
+        localField: 'tweet_id',
+        foreignField: '_id',
+        as: 'tweet',
+      },
+    },
+    { $unwind: { path: '$tweet', preserveNullAndEmptyArrays: true } },
+    {
+      $lookup: {
+        from: 'users',
+        localField: 'tweet.author_id',
+        foreignField: 'user_id',
+        as: 'tweet.author',
+      },
+    },
+    { $unwind: { path: '$tweet.author', preserveNullAndEmptyArrays: true } },
+    {
+      $lookup: {
+        from: 'users',
+        localField: 'follower_id',
+        foreignField: 'user_id',
+        as: 'user',
+      },
+    },
+    { $unwind: { path: '$user', preserveNullAndEmptyArrays: true } },
+  ]);
+
+  return notifiactions;
+};
+
+const getNotificationCount = async (
+  _: any,
+  { lastest_notification_id }: { lastest_notification_id: string },
+  { authUser }: Auth,
+) => {
+  if (!authUser) throw new AuthenticationError('not authenticated');
+
+  const userId = authUser.id;
+
+  const nextNotificationcondition = getNextnotificationsCondition(lastest_notification_id);
   const [count]: Document[] = await notificationModel.aggregate([
     {
       $match: {
-        $and: [{ user_id: userId }, { is_read: false }],
+        $and: [{ user_id: userId }, nextNotificationcondition],
       },
     },
     {
@@ -84,4 +154,4 @@ const getNotificationCount = async (_: any, __: any, { authUser }: Auth) => {
   return count;
 };
 
-export { getNotificationCount, getNotification };
+export { getNotificationCount, getNotification, getNotificationWithMention };
