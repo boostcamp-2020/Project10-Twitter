fabba98a47ef14f6cf5604e944c1b46e7ea4db6e
Baek Jiyoung <bjyoung0@gmail.com>
2020-12-09 23:55:05 +0900

    [refactor] retweet container 변경 - retweet하는 트윗만 보이도록 변경 - 그 외 컴포넌트 제거

--- fe/src/components/organisms/ReTweetContainer/index.tsx
+++ fe/src/components/organisms/ReTweetContainer/index.tsx
@@ -1,13 +1,9 @@
 import React, { FunctionComponent, ReactElement, useState } from 'react';
 import Link from 'next/link';
 import Markdown from 'react-markdown/with-html';
-import MainContaier from '../MainContainer';
 import TitleSubText from '../../molecules/TitleSubText';
-import Button from '../../molecules/Button';
-import { Heart, Comment, Retweet } from '../../atoms/Icons';
 import ProfileImg from '../../atoms/ProfileImg';
-import useHeartState from '../../../hooks/useHeartState';
-import { RetweetBox, ButtonsBox, BodyContainer, HeaderContainer, PinkButton } from './styled';
+import { RetweetBox, BodyContainer, HeaderContainer } from './styled';
 
 interface Props {
   tweet: Tweet;
@@ -20,7 +16,6 @@ interface Tweet {
   retweet_user_number: number;
   heart_user_number: number;
   author: Author;
-  retweet: Tweet;
 }
 interface Author {
   user_id: string;
@@ -28,44 +23,20 @@ interface Author {
   profile_img_url: string;
 }
 
-const ReTweetContainer: FunctionComponent<Props> = ({ tweet }) => {
-  const [isHeart, onClickHeart, onClickUnheart] = useHeartState(tweet);
-
-  return (
-    <MainContaier userId={tweet.author.user_id} ProfileImgUrl={tweet.author.profile_img_url}>
-      <Link href={`/${tweet.author.user_id}`}>
-        <a>
+const ReTweetContainer: FunctionComponent<Props> = ({ tweet }) => (
+  <RetweetBox>
+    <Link href={`/status/${tweet._id}`}>
+      <a>
+        <HeaderContainer>
+          <ProfileImg img={tweet.author.profile_img_url} size={30} />
           <TitleSubText title={tweet.author.name} sub={tweet.author.user_id} />
-        </a>
-      </Link>
-      <Link href={`/status/${tweet._id}`}>
-        <a>
+        </HeaderContainer>
+        <BodyContainer>
           <Markdown allowDangerousHtml>{tweet.content}</Markdown>
-        </a>
-      </Link>
-      <RetweetBox>
-        <Link href={`/status/${tweet.retweet._id}`}>
-          <a>
-            <HeaderContainer>
-              <ProfileImg img={tweet.author.profile_img_url} size={30} />
-              <TitleSubText title={tweet.retweet.author.name} sub={tweet.retweet.author.user_id} />
-            </HeaderContainer>
-            <BodyContainer>
-              <Markdown allowDangerousHtml>{tweet.retweet.content}</Markdown>
-            </BodyContainer>
-          </a>
-        </Link>
-      </RetweetBox>
-      <ButtonsBox component="div">
-        <Button icon={Comment({})} text={tweet.child_tweet_number} />
-        <Button icon={Retweet({})} text={tweet.retweet_user_number} />
-        {isHeart ? (
-          <PinkButton icon={Heart({})} text={tweet.heart_user_number} onClick={onClickUnheart} />
-        ) : (
-          <Button icon={Heart({})} text={tweet.heart_user_number} onClick={onClickHeart} />
-        )}
-      </ButtonsBox>
-    </MainContaier>
-  );
-};
+        </BodyContainer>
+      </a>
+    </Link>
+  </RetweetBox>
+);
+
 export default ReTweetContainer;
