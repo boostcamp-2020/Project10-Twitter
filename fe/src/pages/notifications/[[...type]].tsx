6ba55d89486d2d4f8da2a4685f2a89facb5c14c5
Jae-Woo Joo <wnwodn4293@naver.com>
2020-12-10 20:54:41 +0900

    [feat] 알림 페이지 무한 스크롤 및 캐시 업데이트
    - 알림 페이지 무한 스크롤 구현
    - 읽음 표시로 보내는 mutation 후 캐시 업데이트

--- fe/src/pages/notifications/[[...type]].tsx
+++ fe/src/pages/notifications/[[...type]].tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useEffect } from 'react';
+import React, { FunctionComponent, useEffect, useState, useRef } from 'react';
 import { useMutation, useQuery } from '@apollo/client';
 import { useRouter } from 'next/router';
 import TabBar from '../../components/molecules/TabBar';
@@ -8,6 +8,8 @@ import GET_MYINFO from '../../graphql/getMyInfo.gql';
 import GET_NOTIFICATION from '../../graphql/getNotification.gql';
 import GET_MENTION_NOTIFICATION from '../../graphql/getMentionNotification.gql';
 import UPDATE_NOTIFICATION from '../../graphql/updateNotification.gql';
+import useInfiniteScroll from '../../hooks/useInfiniteScroll';
+import apolloClient from '../../libs/apolloClient';
 
 interface QueryVariable {
   variables: Variable;
@@ -50,13 +52,24 @@ interface Author {
   profile_img_url: string;
 }
 
+const getValue = (type: string | string[] | undefined) => {
+  if (!type) return 'all';
+  return 'mention';
+};
+
 const Notification: FunctionComponent = () => {
   const router = useRouter();
   const { type } = router.query;
   const queryArr = { all: GET_NOTIFICATION, mention: GET_MENTION_NOTIFICATION };
-  const value = type ? type[0] : 'all';
-  const { data } = useQuery(queryArr[value]);
+  const value = getValue(type);
+  // type ? type[0] : 'all';
+  const { data, fetchMore } = useQuery(queryArr[value]);
   const [mutate] = useMutation(UPDATE_NOTIFICATION);
+
+  const [notificationList, setNotificationList] = useState<Noti[]>([]);
+  const { _id: bottomNotificationId } = notificationList[notificationList.length - 1] || {};
+  const fetchMoreEl = useRef(null);
+  const [intersecting, loadFinished, setLoadFinished] = useInfiniteScroll(fetchMoreEl);
 
   const onClick = (e: React.SyntheticEvent<EventTarget>) => {
     const target = e.target as HTMLInputElement;
@@ -68,6 +81,25 @@ const Notification: FunctionComponent = () => {
   };
 
   useEffect(() => {
+    const asyncEffect = async () => {
+      if (!intersecting || loadFinished || !bottomNotificationId || !fetchMore) return;
+      const { data: newData } = await fetchMore({
+        variables: { id: bottomNotificationId },
+      });
+      if (newData.notifications.length < 20) setLoadFinished(true);
+    };
+    asyncEffect();
+  }, [intersecting]);
+
+  useEffect(() => {
+    apolloClient.cache.evict({ id: 'ROOT_QUERY', fieldName: 'notification_list' });
+    apolloClient.cache.evict({ id: 'ROOT_QUERY', fieldName: 'notification_mention_list' });
+  }, []);
+
+  useEffect(() => {
+    if (data?.notifications) {
+      setNotificationList(data?.notifications);
+    }
     const lastestNotification = data?.notifications[0];
     if (lastestNotification)
       mutate({
@@ -87,13 +119,12 @@ const Notification: FunctionComponent = () => {
   return (
     <PageLayout>
       <TabBar value={value} handleChange={onClick} labels={['all', 'mention']} />
-      {data ? (
-        data.notifications?.map((noti: Noti, index: number) => (
-          <NotificationContainer noti={{ ...noti, curTabValue: value }} />
-        ))
-      ) : (
-        <>loading..</>
-      )}
+      <>
+        {notificationList?.map((noti: Noti, index: number) => (
+          <NotificationContainer key={index} noti={{ ...noti, curTabValue: value }} />
+        ))}
+      </>
+      <div ref={fetchMoreEl} />
     </PageLayout>
   );
 };
