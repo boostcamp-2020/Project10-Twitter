5324ec9d22e193458c502ec75fdae820108629de
Baek Jiyoung <bjyoung0@gmail.com>
2020-12-11 09:52:07 +0900

    [feat] signup modal 수정 - input container css 설정 - useOnTextChange 설정 - 버튼 클릭 시 회원 가입 기능 구현

--- fe/src/components/organisms/SignupModal/index.tsx
+++ fe/src/components/organisms/SignupModal/index.tsx
@@ -1,7 +1,9 @@
 import React, { FunctionComponent } from 'react';
-import { useQuery } from '@apollo/client';
+import { useMutation } from '@apollo/client';
 import Modal from '../../molecules/Modal';
-import InputContainer from '../../molecules/InputContainer';
+import { StyledInputContainer, StyledButton } from './styled';
+import ADD_USER from '../../../graphql/addUser.gql';
+import useOnTextChange from '../../../hooks/useOnTextChange';
 
 interface Props {
   displayModal: boolean;
@@ -17,11 +19,29 @@ interface User {
 }
 
 const HeartListModal: FunctionComponent<Props> = ({ displayModal, onClickCloseBtn }) => {
+  const [createUser, { loading: mutationLoading, error: mutationError }] = useMutation(ADD_USER);
+  const [userId, setUserId, onUserIdChange] = useOnTextChange('');
+  const [name, setName, onNameChange] = useOnTextChange('');
+  const [password, setPassword, onPasswordChange] = useOnTextChange('');
+
+  const onSignupBtnClick = () => {
+    createUser({ variables: { userId, name, password } });
+    setUserId('');
+    setName('');
+    setPassword('');
+  };
+
   return (
     <Modal displayModal={displayModal} onClickCloseBtn={onClickCloseBtn}>
-      <InputContainer labelValue="아이디" />
-      <InputContainer labelValue="이름" />
-      <InputContainer labelValue="비밀번호" type="password" />
+      <StyledInputContainer labelValue="아이디" inputValue={userId} onChange={onUserIdChange} />
+      <StyledInputContainer labelValue="이름" inputValue={name} onChange={onNameChange} />
+      <StyledInputContainer
+        labelValue="비밀번호"
+        type="password"
+        inputValue={password}
+        onChange={onPasswordChange}
+      />
+      <StyledButton text="가입" color="primary" variant="contained" onClick={onSignupBtnClick} />
     </Modal>
   );
 };
