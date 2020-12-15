3a739792e4172ad7649d29b67d8922ae27d85622
Jae-Woo Joo <wnwodn4293@naver.com>
2020-12-15 22:36:57 +0900

    [refactor] 매직넘버 제거
    - 60 *1000을 ONE_MINUTE 상수로 선언

--- fe/src/components/organisms/SideBar/index.tsx
+++ fe/src/components/organisms/SideBar/index.tsx
@@ -10,6 +10,8 @@ import { NewTweetModal } from '@organisms';
 import { GET_NOTIFICATION_COUNT } from '@graphql/notification';
 import Container from './styled';
 
+const ONE_MINUTE = 60 * 1000;
+
 interface ButtonProps {
   id: number;
   text: string;
@@ -55,7 +57,7 @@ const SideBar: FunctionComponent = () => {
   const variables = { id: myProfile ? myProfile.lastest_notification_id : undefined };
   const { data } = useQuery(GET_NOTIFICATION_COUNT, {
     variables,
-    pollInterval: 60 * 1000,
+    pollInterval: ONE_MINUTE,
   });
 
   const onKeyDown = (e: any) => {
